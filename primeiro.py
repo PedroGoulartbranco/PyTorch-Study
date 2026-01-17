c2d3565a66027ff7a40b56e0cc77c429a97b3370
Pedro Goulart Branco <72553813+PedroGoulartbranco@users.noreply.github.com>
2026-01-17 17:38:48 -0300

    Update primeiro.py

--- primeiro.py
+++ primeiro.py
@@ -1,64 +1,30 @@
-import torch
-import numpy as np
-from torch import nn
+#Fazer um neuronio aprender y = 2x + 3
 
-class LineNetwork(nn.Module): #Importa modelo do torch
-    def __init__(self): #Inicialização
-        super().__init__()
-        self.layers = nn.Sequential( #Faz a conta
-            nn.Linear(1, 1)
-        )
+#Ideia simples -> chute =  peso . x + bias
 
-    #Como a rede computa
-    def forward(self, x): #O x é o dado que entra
-        return self.layers(x) #O x entra dentro da conta, (x * pesos + bias)
-    
-from torch.utils.data import Dataset, DataLoader
-import torch.distributions.uniform as urand #Criar numeros aleatorios de forma uniforme
+import math
+import random
 
-class AlgebraDataset(Dataset):
-    def __init__(self, f, interval, nsamples):
-        X = urand.Uniform(interval[0], interval[1]).sample(nsamples) #Cria numeros aleatorios onde todos 
-        #Onde todos tem chance igual de ir, o intervalo a pessoa vai passar, o sample é quantidade desses numeros
-        #o nsamples é a pessoa que passa se quer por exemplo 10 dados,
-        #Uniform = todos numeros tem chances iguais de cair
+dados = [
+    (1, 5),
+    (2, 7),
+    (3, 9),
+    (4, 11)
+]
 
-        self.data = [(x, f(x)) for x in X] #a pessoa passa a formula que é o f, o x menor é um dos dados acima
-        #que fora aleatorizados
+peso = random.uniform(-1, 1)
+bias = random.uniform(-1, 1)
+taxa_aprendizagem = 0.01 #Quanto que vai mudar depois de cada tentativa, quanto menor mais demora mais menos explode ;)
 
-    def __len__(self):
-        return len(self.data) #Quantidade de dados
-    
-    def __getitem__(self, indice):
-        return self.data[indice] #Retorna dado especifico por indice
-    
-reta = lambda x: 2*x + 3
-intervalo = (-10, 10)
-numero_amostras = 1000 
-teste_numero_amostras = 100
+for i in range(500):
+    for x, y in dados:
+        chute = peso * x + bias
+        
+        erro = y - chute
 
-train_dataset = AlgebraDataset(reta, intervalo, numero_amostras)
-teste_train_dataset = AlgebraDataset(reta, intervalo, teste_numero_amostras)
-
-train_dataloader = DataLoader(train_dataset,batch_size= numero_amostras, shuffle=True)
-teste_train_dataloader = DataLoader(teste_train_dataset,batch_size= teste_numero_amostras, shuffle=True)
-
-funcao_distancia = nn.MSELoss() #Função de perda, erro quadratico metrico
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(device)
-
-model = LineNetwork().to(device)
-
-#SGD = Stochastic  Gradient Descent
-# Ajusta os pesos e o bias usando o gradiente
-# para fazer o erro diminuir a cada passo
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-#lr = taxa de aprendizado
-
-def treinar(model,dataloader, funcao_distancia, optmizer):
-    model.train()
-
-    for X, y in dataloader:
-        X = X.to(device)
-        y = y.to(device)
+        peso += erro * x * taxa_aprendizagem #Multiplica o X porque na formula o peso é multiplicado pelo X
+        bias += erro * taxa_aprendizagem
+    if i % 10 == 0:
+        print(f"Número: {i}\nPesos: {peso}\nBias: {bias}\nErro: {erro}")
+        print(f"Chute: {chute}\nValor Real: {y}")
+        print("-=" * 10)
