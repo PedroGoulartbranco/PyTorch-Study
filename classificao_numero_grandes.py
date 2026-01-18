9cd4b8dc89601b743f26b78fb0ba9493f7b94ca3
Pedro Goulart Branco <72553813+PedroGoulartbranco@users.noreply.github.com>
2026-01-18 11:53:38 -0300

    trocaundo relu para sigmoid

--- classificao_numero_grandes.py
+++ classificao_numero_grandes.py
@@ -19,23 +19,37 @@ pesos = random.uniform(-1, 1)
 bias = random.uniform(-1, 1)
 taxa_aprendizado = 0.01
 
-def relu(x):
-    return max(0, x)
+def sigmoid(x):
+    #O uso do max e min é para nao ficar gigante os números, se passar de -500 por exe -1000 fica -500 e se passar de 500 fica 500
+    return 1 / (1 + math.exp(-max(-500, min(500, x))))
 
 
-for i in range(1000):
+for i in range(2000):
     for numero, binario in dados:
         chute = pesos * numero + bias
-        ativacao = relu(chute)
+        probabilidade = sigmoid(chute)
 
-        erro = binario - ativacao
+        erro = binario - probabilidade
 
         pesos += erro * numero * taxa_aprendizado
         bias += erro * taxa_aprendizado
     
     if (i % 10 == 0):
-        print(f"Número: {numero}\nChute: {ativacao}")
+        print(f"Número: {numero}\nChute: {probabilidade * 100:.1f}%")
 
+def pequeno_grande(numero):
+    formula = pesos * numero + bias
+    numero = sigmoid(formula)
+    return (numero * 100)
 
-teste = int(input("Digite um número: "))
-print(f"Maior ou menor: {pesos * teste + bias}")
+contador = 0
+
+while True:
+    contador += 1
+    numero = int(input("Digite um número: "))
+    print(f"{numero} é grande ? {pequeno_grande(numero):.1f}% de ser grande")
+    print("-=" * 20)
+    if contador % 5 == 0:
+        sair = str(input("Sair ? [S/N] ")).upper()
+        if sair == "S":
+            break
